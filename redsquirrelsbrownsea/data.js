e56566e11adbbec0a73381cbbbebe9b596a51770
Kelvin Hill <kelvin.a.hill@gmail.com>
2024-10-18 18:52:41 +0100

    Signed-off-by: Kelvin Hill <kelvin.a.hill@gmail.com>

--- redsquirrelsbrownsea/data.js
+++ redsquirrelsbrownsea/data.js
@@ -1,8 +1,8 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-red-squirrels--index",
-      "name": "Red Squirrels – Index",
+      "id": "0-red-squirrels",
+      "name": "Red Squirrels",
       "levels": [
         {
           "tileSize": 256,
