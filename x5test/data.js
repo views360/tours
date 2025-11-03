a5490d320da060a250d16cfd8a29f1f95d7775fc
Views 360 <120094358+views360@users.noreply.github.com>
2025-11-03 10:08:20 +0000

    Added X5 Test
    Commit 03 Nov

---
+++ x5test/data.js
@@ -0,0 +1,46 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-teddy-field",
+      "name": "teddy-field",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 2.956555391983745,
+        "pitch": 0.5801340999288609,
+        "fov": 1.5202647436324659
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
